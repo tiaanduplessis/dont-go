cb0aaed3b942093fa2cb9e6303cd6f6e41a2c1f8
Tiaan <tiaanduplessis@hotmail.com>
2017-02-23 22:42:46 +0200

    Update & cleanup build process

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,49 +1,33 @@
-import webpack from 'webpack';
-import path from 'path';
+import path from 'path'
+import packageConfig from './package'
 
-// Package.json
-import packageConfig from './package';
-const { name: libraryName, filename: libraryFilename, entry: libraryEntry } = packageConfig.library;
-
-// Environment variable
-const env = process.env.NODE_ENV || 'production';
-const PROD = env === 'production';
-
-// Configuration
-const outputPath = path.resolve( __dirname, 'lib' );
-const outputName = PROD ? `${libraryFilename}.min.js` : `${libraryFilename}.js`;
+const { name: libraryName, filename: libraryFilename } = packageConfig.library
+const outputName = process.env.NODE_ENV === 'production'
+  ? `${libraryFilename}.min.js`
+  : `${libraryFilename}.js`
 
 export default {
-  entry: libraryEntry,
-  devtool: 'source-map',
+  entry: path.join(__dirname, '/src/index.js'),
+  devtool: 'cheap-module-source-map',
   output: {
-    path: outputPath,
-    library: libraryName,
+    path: path.join(__dirname, '/lib'),
     filename: outputName,
+    library: libraryName,
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
-  resolve: {
-    root: path.resolve( 'src' ),
-    extensions: ['', '.js']
-  },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.js$/,
-        loader: 'babel?cacheDirectory',
-        include: path.resolve( 'src' )
-      },
-      {
-        test: /\.js$/,
-        loader: 'eslint',
-        include: path.resolve( 'src' )
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        include: path.resolve('src'),
+        exclude: /(node_modules|bower_components)/
       }
     ]
   },
-  plugins: PROD ? [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false }
-    })
-  ] : []
-};
+  resolve: {
+    modules: [path.resolve('./src')],
+    extensions: ['.json', '.js']
+  }
+}
